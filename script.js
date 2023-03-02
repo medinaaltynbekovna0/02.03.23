09d26289642eb6d232575be063aa8ef8ea696fab
medina <damirovamedina38@gmail.com>
2023-03-02 15:30:24 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const ul = document.createElement('ul');
+document.querySelector('body').prepend(ul);
+const exampleList = ["Apples", "Pears", "Oranges"];
+for (let i= 0; i < exampleList.length; i++){
+const element = exampleList[i];
+const li = document.createElement('li');
+li.textContent = element
+ul.prepend(li);
+
+}
+const exampleList2 = [
+	{name:"Bakyt", age: 18},
+	{name: "Dosbol", age: 32}
+]
+
+const h1 = document.querySelector("h1");
+document.querySelector("#display-text").addEventListener("input",
+function(){
+	h1.textContent = this.value;
+});
+
+document.querySelector("#font-size").addEventListener("input",
+function(){
+	h1.style.fontSize = this.value +'px';
+});
+
